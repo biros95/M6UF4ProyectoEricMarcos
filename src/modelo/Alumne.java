d096aee8532acd7c2b3aff0c8d693c5f3d37a46d
ericdote <rovx15@gmail.com>
2017-04-20 20:45:51 +0200

    Persistence configurado.

--- src/modelo/Alumne.java
+++ src/modelo/Alumne.java
@@ -5,11 +5,27 @@
  */
 package modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author MarcosPortatil
  */
-public class Alumne {
+@Entity
+@Table(name ="Alumnes")
+public class Alumne implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "clientId", unique = true, nullable = false)
     private int id;
     private String nom;
     private String cognom;
