f476e6803c56452ce489b0931ba638c540357e04
ericdote <rovx15@gmail.com>
2017-04-20 21:08:10 +0200

    Modelo hecho "basico"

--- src/modelo/Import.java
+++ src/modelo/Import.java
@@ -5,18 +5,35 @@
  */
 package modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Eric
  */
-public class Import {
-    
-    private double importe;
-    private String matricula;
+@Entity
+@Table(name = "Import")
+public class Import implements Serializable {
 
-    public Import(double importe, String matricula) {
-        this.importe = importe;
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "importId", unique = true, nullable = false)
+    private int matricula;
+
+    @Column(name = "import", nullable = false)
+    private double importe;
+
+    public Import(int matricula, double importe) {
         this.matricula = matricula;
+        this.importe = importe;
     }
 
     public double getImporte() {
@@ -27,14 +44,44 @@ public class Import {
         this.importe = importe;
     }
 
-    public String getMatricula() {
+    public int getMatricula() {
         return matricula;
     }
 
-    public void setMatricula(String matricula) {
+    public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + this.matricula;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Import other = (Import) obj;
+        if (this.matricula != other.matricula) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Import{" + "matricula=" + matricula + ", importe=" + importe + '}';
+    }
+
     
-    
-    
+
 }
