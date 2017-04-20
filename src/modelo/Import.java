9b2bf1a23d423c2347d8d97b207e749fdda602eb
ericdote <rovx15@gmail.com>
2017-04-20 19:32:00 +0200

    Initial Commit

---
+++ src/modelo/Import.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Eric
+ */
+public class Import {
+    
+    private double importe;
+    private String matricula;
+
+    public Import(double importe, String matricula) {
+        this.importe = importe;
+        this.matricula = matricula;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+    
+    
+}
